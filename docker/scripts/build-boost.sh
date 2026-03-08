231551409fdccc4ab2f49d2526b83bf95bbd5316
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2026-03-08 15:18:56 +0100

    Add fallback download URL for Boost source in build-boost.sh

--- docker/scripts/build-boost.sh
+++ docker/scripts/build-boost.sh
@@ -6,6 +6,9 @@ scripts_path=$(dirname "$(readlink -f "$0")")
 source "${scripts_path}/common.sh"
 
 if [ ! -f "boost_${BOOST_VERSION_FILE}.tar.bz2" ]; then
+  wget -q https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_FILE}.tar.bz2 || true
+fi
+if [ ! -f "boost_${BOOST_VERSION_FILE}.tar.bz2" ]; then
   wget -q https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_FILE}.tar.bz2
 fi
 set -ex
